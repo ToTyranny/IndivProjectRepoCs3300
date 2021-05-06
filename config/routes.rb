b92772d97805a917af71a3e3d53eefe5256b3747
root <totyranny@hotmail.com>
2021-05-06 00:57:08 +0000

    Added scaffolding

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :projects
   root 'welcome#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
+Rails.application.routes.draw do
+  root "projects#index"
+  resources :projects
 end
